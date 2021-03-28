2e5b37537d2caaee7da01e137809b40361d670f3
Shilong Li <lishilong1108@foxmail.com>
2021-03-28 17:34:28 +0800

    SpringMVC执行的流程

---
+++ SeasonOne/src/main/java/com/tx/SpringMVCRunningProcess.java
@@ -0,0 +1,9 @@
+package com.tx;
+
+/**
+ * 发送请求--DispatcherServlet中央处理器--调用处理器映射器--返回HandlerExecutionChain（返回所有的处理器拦截器对象）
+ * --通过处理器适配器调用具体的处理器（具体的Controller）--返回ModelAndView给DispatcherServlet--
+ * 调用视图解析器--返回view对象--渲染视图--响应用户
+ */
+public class SpringMVCRunningProcess {
+}
