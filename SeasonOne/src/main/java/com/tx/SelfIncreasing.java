a109cca6cf0755ddf49a2fe822c667745098435c
Shilong Li <lishilong1108@foxmail.com>
2021-03-28 11:45:59 +0800

    自增变量

---
+++ SeasonOne/src/main/java/com/tx/SelfIncreasing.java
@@ -0,0 +1,19 @@
+package com.tx;
+
+public class SelfIncreasing {
+    public static void main(String[] args) {
+        int i = 1;
+        // 先算i++,i=2,i++=1,覆盖，i=1
+        i = i++;
+        // j = 1,i = 2
+        int j = i++;
+        // k = 2+3*3,i = 4
+        int k = i + ++i * i++;
+        // 4
+        System.out.println("i="+ i);
+        // 1
+        System.out.println("j="+ j);
+        // 11
+        System.out.println("k="+ k);
+    }
+}
