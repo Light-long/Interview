12287090b04e6b53da2426f5e76e919a3f96b7c4
Shilong Li <lishilong1108@foxmail.com>
2021-03-31 20:54:25 +0800

    ConcurrentHashMap

---
+++ SeasonTwo/src/main/java/com/tx/safecollection/ConcurrentHashMapDemo.java
@@ -0,0 +1,12 @@
+package com.tx.safecollection;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * HashMap的安全类ConcurrentHashMap
+ */
+public class ConcurrentHashMapDemo {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+    }
+}
